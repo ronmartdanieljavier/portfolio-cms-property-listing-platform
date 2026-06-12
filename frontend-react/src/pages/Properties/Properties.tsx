3a70c93ac121e6839519d6f20b56e92c7546c66b
Ron Mart Daniel Javier <ronmartdanieljavier@gmail.com>
2026-06-12 19:21:04 +1000

    chore(fontend-check): fix fontend checks

--- frontend-react/src/pages/Properties/Properties.tsx
+++ frontend-react/src/pages/Properties/Properties.tsx
@@ -35,7 +35,11 @@ export default function Properties() {
   function toggleExpanded(id: number) {
     setExpanded((prev) => {
       const next = new Set(prev);
-      next.has(id) ? next.delete(id) : next.add(id);
+      if (next.has(id)) {
+        next.delete(id);
+      } else {
+        next.add(id);
+      }
       return next;
     });
   }
